4fbc6751786459ef7025c4bfc927d98709bf01ae
Jannatul Ferdous <jannatpinky92@gmail.com>
2021-03-13 20:16:28 -0600

    Final Project

---
+++ fs1010-course-project-client/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './App.css'
+import Navigation from './components/shared/Navigation'
+import Footer from './components/shared/footer'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+import Contact from './components/pages/Contact'
+import Skills from './components/pages/Skills'
+import Login from './components/pages/Login'
+import Listing from './components/pages/Listing'
+import PrivateRoute from './components/shared/PrivateRoute'
+
+function App() {
+  return (
+   <BrowserRouter>
+        <Navigation />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/Skills" component={Skills} />
+          <PrivateRoute path="/submissions">
+            <Listing />
+          </PrivateRoute>
+        </Switch>
+        <Footer />  
+    </BrowserRouter>
+  )
+}
+
+export default App;
